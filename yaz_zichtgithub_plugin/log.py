9bb83975d5a3dc3b5c4b3e5396cd5653e3cc8fff
boudewijn <boudewijn@sniper>
2017-11-27 15:56:10 +0100

    Use coloredlogs whenever available

--- yaz_zichtgithub_plugin/log.py
+++ yaz_zichtgithub_plugin/log.py
@@ -7,8 +7,13 @@ __all__ = ["logger", "set_verbose"]
 # Name the logger after the package.
 logger = logging.getLogger(__package__)
 
-
 def set_verbose(verbose: bool):
     """Set the logging level to INFO when VERBOSE"""
     if verbose:
         logging.basicConfig(level=logging.INFO)
+
+        try:
+            import coloredlogs
+            coloredlogs.install(level="INFO")
+        except:
+            pass
